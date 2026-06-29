d8f5c539abdf70e443eb33f49ed05edaef33b5bd
fidhafathima693 <fidhafathima693@gmail.com>
2026-06-29 09:51:28 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const envelope = document.getElementById("envelope");
+const opening = document.getElementById("opening");
+const mainContent = document.getElementById("mainContent");
+const music = document.getElementById("bgMusic");
 
+mainContent.style.display = "none";
+
+envelope.addEventListener("click", () => {
+
+    envelope.classList.add("open");
+
+    music.play().catch(() => {});
+
+    setTimeout(() => {
+
+        opening.style.opacity = "0";
+        opening.style.transition = "1s";
+
+    },1200);
+
+    setTimeout(() => {
+
+        opening.style.display = "none";
+        mainContent.style.display = "block";
+
+        document.body.style.overflowY = "auto";
+
+    },2200);
+
+});
